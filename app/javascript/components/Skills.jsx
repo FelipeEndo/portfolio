a10e77fb2e400f2e8b334cdf9fc4ef727dd5c67b
Felipe Endo <felipecs.endo@gmail.com>
2024-05-15 17:21:12 -0300

    add skills section

--- app/javascript/components/Skills.jsx
+++ app/javascript/components/Skills.jsx
@@ -8,56 +8,105 @@ export default function Skills() {
   return (
     <div>
       <h1> Skills </h1>
-      <Row xs={1} md={2} className="g-4">
-        {Array.from({ length: 4 }).map((_, idx) => (
-          <Col key={idx}>
-            <Card>
-              <Card.Img variant="center" src="react-2.svg" height="70" width="70" className="mx-auto" />
-              <Card.Body>
-                <Card.Title>ReactJs</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-      <CardGroup className="border-0">
-        <Card className="justify-content-center">
-          <Card.Img variant="horizontal" src="react-2.svg" height="70" width="70" className="mx-auto" />
-          <Card.Body>
-            <Card.Title>ReactJs</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card className="justify-content-right px-auto">
-          <Card.Img variant="center" src="ruby.svg" height="70" width="70" className="mx-auto"/>
-          <Card.Body>
-            <Card.Title className="justify-content-center">Ruby</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="center" src="rails-1.svg" height="70" width="100" className="mx-auto" />
-          <Card.Body>
-            <Card.Title>Rails</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="center" src="elixir-1.svg" width="120" className="mx-auto" />
-          <Card.Body>
-            <Card.Title>Elixir</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="center" src="docker.svg" height="70" width="70" className="mx-auto" />
-          <Card.Body>
-            <Card.Title>Docker</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="center" src="vue-9.svg" height="70" width="70" className="mx-auto pt-2" />
-          <Card.Body>
-            <Card.Title>VueJs</Card.Title>
-          </Card.Body>
-        </Card>
-      </CardGroup>
+        <div className="justify-content-center mt-5" style={{ display: 'flex' }}>
+          <Card className="mx-2" style={{ width: '60rem' }}>
+            <Row className='g-0'>
+              <Col md='4'>
+                <Card.Img src="react-2.svg" height="100" width="100" className="my-4" />
+              </Col>
+              <Col md='8'>
+                <Card.Body>
+                  <Card.Title>React</Card.Title>
+                  <Card.Text>
+                    See some of my projects using React
+                  </Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+
+          <Card className="mx-2" style={{ width: '60rem' }}>
+            <Row className='g-0'>
+              <Col md='4'>
+                <Card.Img src="react-2.svg" height="100" width="100" className="mx-auto mt-4" />
+              </Col>
+              <Col md='8'>
+                <Card.Body>
+                  <Card.Title>React</Card.Title>
+                  <Card.Text>
+                    See some of my projects using React
+                  </Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+
+          <Card className="mx-2" style={{ width: '60rem' }}>
+            <Row className='g-0'>
+              <Col md='4'>
+                <Card.Img src="react-2.svg" height="100" width="100" className="mx-auto mt-4" />
+              </Col>
+              <Col md='8'>
+                <Card.Body>
+                  <Card.Title>React</Card.Title>
+                  <Card.Text>
+                    See some of my projects using React
+                  </Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        </div>
+
+        <div className="justify-content-center mt-5" style={{ display: 'flex' }}>
+          <Card className="mx-2" style={{ width: '60rem' }}>
+            <Row className='g-0'>
+              <Col md='4'>
+                <Card.Img src="react-2.svg" height="100" width="100" className="my-4" />
+              </Col>
+              <Col md='8'>
+                <Card.Body>
+                  <Card.Title>React</Card.Title>
+                  <Card.Text>
+                    See some of my projects using React
+                  </Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+
+          <Card className="mx-2" style={{ width: '60rem' }}>
+            <Row className='g-0'>
+              <Col md='4'>
+                <Card.Img src="react-2.svg" height="100" width="100" className="mx-auto mt-4" />
+              </Col>
+              <Col md='8'>
+                <Card.Body>
+                  <Card.Title>React</Card.Title>
+                  <Card.Text>
+                    See some of my projects using React
+                  </Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+
+          <Card className="mx-2" style={{ width: '60rem' }}>
+            <Row className='g-0'>
+              <Col md='4'>
+                <Card.Img src="react-2.svg" height="100" width="100" className="mx-auto mt-4" />
+              </Col>
+              <Col md='8'>
+                <Card.Body>
+                  <Card.Title>React</Card.Title>
+                  <Card.Text>
+                    See some of my projects using React
+                  </Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        </div>
     </div>
   );
 }
