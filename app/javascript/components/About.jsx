fab77661566913703cde238be441081da458f07c
Felipe Endo <felipecs.endo@gmail.com>
2024-05-16 02:12:31 -0300

    change skills and projects layout

--- app/javascript/components/About.jsx
+++ app/javascript/components/About.jsx
@@ -6,8 +6,8 @@ import { Link } from "react-router-dom";
 export default function About() {
   return (
   <div>
-    <h1> About </h1>
-    <div className="justify-content-center mt-5" style={{ display: 'flex' }}>
+    <h1 className="text-center text-white pt-5"> About </h1>
+    <div className="justify-content-center mt-5 pb-5" style={{ display: 'flex' }}>
       <AboutPhotoCard />
       <AboutDescriptionCard />
     </div>
